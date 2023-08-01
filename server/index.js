7d44a6c892cf894f93d85a5db562d644c9014cdf
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2023-08-01 21:31:18 +0530

    feat: mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,45 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
+
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
 //handler
-app.post('/api/register', (req, res) => {
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    res.json({status: 'ok'})
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password,
+    })
+
+    if (user) {
+        return res.json({ status: 'ok', user: true })
+    } else {
+        return res.json({ status: 'error', user: false })
+    }
 })
 
 app.listen(1337, () => {
