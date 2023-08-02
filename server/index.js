cc7556670fbace755c6c95f442460d9d75da5e29
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2023-08-02 12:06:20 +0530

    feat: password hashing

--- server/index.js
+++ server/index.js
@@ -4,8 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
-require('dotenv').config();
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -13,68 +14,67 @@ app.use(express.json())
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+});
 
-//middleware to authenticate token 
+//middleware to authenticate token
 // Middleware to authenticate token
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        return res.sendStatus(401); // Unauthorized
+        return res.sendStatus(401) // Unauthorized
     }
 
     jwt.verify(token, 'secret123', (err, user) => {
         if (err) {
             return res.sendStatus(403); // Forbidden
         }
-        req.user = user; // Store the user data in the request object
-        next();
+        req.user = user // Store the user data in the request object
+        next()
     });
 }
 
 //handler
 app.post('/api/register', async (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); // Hash the password
         await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
+            password: hashedPassword, // Store the hashed password
+        });
         res.json({ status: 'ok' })
     } catch (err) {
         console.log(err)
         res.json({ status: 'error', error: 'Duplicate email' })
     }
-})
+});
 
 app.post('/api/login', async (req, res) => {
     const user = await User.findOne({
         email: req.body.email,
-        password: req.body.password,
-    })
+    });
 
-    if (user) {
+    if (user && await bcrypt.compare(req.body.password, user.password)) {
         const token = jwt.sign({
             name: user.name,
             email: user.email,
-        },
-            'secret123'
-        )
-        return res.json({ status: 'ok', user: token })
-    } else {
-        return res.json({ status: 'error', user: false })
-    }
-})
+        }, 'secret123')
 
-//dashboard only visible to those who have authenticated token 
+        return res.json({ status: 'ok', user: token });
+    } else {
+        return res.json({ status: 'error', user: false });
+    }
+});
+
+//dashboard only visible to those who have authenticated token
 app.get('/dashboard', authenticateToken, (req, res) => {
     // The user is authenticated, you can access req.user for user data
     res.json({ message: 'Welcome to the dashboard!', user: req.user });
 });
 
 app.listen(1337, () => {
-    console.log('Server started on 1337')
-})
+    console.log('Server started on 1337');
+});
