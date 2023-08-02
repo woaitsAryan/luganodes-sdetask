31ab4fc4518c12ee1d1163364f45b055fc6bfa02
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2023-08-02 13:46:38 +0530

    feat: Login using metamask

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { ethers } from 'ethers';
 
 function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // State to track Metamask login
+    const [metamaskLoggedIn, setMetamaskLoggedIn] = useState(false);
 
     async function loginUser(event) {
-        event.preventDefault()
+        event.preventDefault();
         const response = await fetch('http://localhost:1337/api/login', {
             method: 'POST',
             headers: {
@@ -15,22 +19,56 @@ function Login() {
                 email,
                 password,
             }),
-        })
+        });
 
-        const data = await response.json()
+        const data = await response.json();
         if (data.user) {
-            alert('Login successful')
-            window.location.href = '/dashboard'
+            alert('Login successful');
+            window.location.href = '/dashboard';
         } else {
-            alert("pls check ur email and pw")
+            alert('Please check your email and password.');
         }
-        console.log(data)
+    }
+
+    async function loginWithMetamask() {
+        if (window.ethereum) {
+            try {
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                await provider.send("eth_requestAccounts", []); // Request permission
+                const signer = provider.getSigner();
+                const address = await signer.getAddress();
+
+                const response = await fetch('http://localhost:1337/api/login/metamask', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        address,
+                    }),
+                });
+
+                const data = await response.json();
+                if (data.user) {
+                    alert('Metamask login successful');
+                    setMetamaskLoggedIn(true);
+                    window.location.href = '/dashboard';
+                } else {
+                    alert('Metamask login failed');
+                }
+            } catch (error) {
+                console.error('Metamask login error:', error);
+            }
+        } else {
+            alert('Metamask is not installed or not accessible');
+        }
     }
 
     return (
         <div>
             <h1>Login</h1>
             <form onSubmit={loginUser}>
+                {/* Email/password fields */}
                 <input
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
@@ -45,12 +83,14 @@ function Login() {
                     placeholder="Password"
                 />
                 <br />
-                <input
-                    type="submit"
-                    value="Login"
-                />
-
+                <input type="submit" value="Login" />
             </form>
+
+            {/* Metamask login */}
+            {!metamaskLoggedIn && (
+                <button onClick={loginWithMetamask}>Login with Metamask</button>
+            )}
+            {metamaskLoggedIn && <p>Logged in with Metamask</p>}
         </div>
     );
 }
